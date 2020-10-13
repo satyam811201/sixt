d6e1fc3ed1f612e1cca624d97ba09994e8066d78
satyam811201 <satyam.bharti99@gmail.com>
2020-10-13 08:49:21 +0530

    pom commit

---
+++ src/main/java/sixtproject/sixtproject/App.java
@@ -0,0 +1,13 @@
+package sixtproject.sixtproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
